6b140da30f910cf994768adc1cf72f4f49c7e3a0
Nidhi Kumari <nidhi843330@gmail.com>
2023-03-26 16:00:53 +0530

    json added

---
+++ ap.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use('/api', router);
+server.listen(process.env.PORT || 3001, () => {
+  console.log('JSON Server is running')
+});
+
+module.exports = server.createHandler({
+  path: '/api'
+});
